a1a717d887a789ff75a2d435cafa98facf959b97
Cash Myers <cmyers880@gmail.com>
2018-01-18 16:47:19 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require('./db/mongoose');
+
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
@@ -112,7 +113,7 @@ app.post('/users', (req, res) => {
   }).then((token) => {
     res.header('x-auth', token).send(user);
   }).catch((e) => {
-    res.status(400).send(e);
+    res.status(400).send();
   });
 });
 
@@ -120,6 +121,19 @@ app.post('/users', (req, res) => {
 // ----- GET /users/me -----// [PRIVATE]
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+// ----- POST /users/login {email, password} ----- //
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
 });
 
 // ----- Activate listener ----- //
